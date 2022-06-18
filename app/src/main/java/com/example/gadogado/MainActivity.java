029f2e8f4b66f1dfc36b54c9f9e520ecadcdb264
priskilladrn <pikapriskilla6773@gmail.com>
2022-06-18 23:26:10 +0700

    bottom navbar

--- app/src/main/java/com/example/gadogado/MainActivity.java
+++ app/src/main/java/com/example/gadogado/MainActivity.java
@@ -1,17 +1,62 @@
 package com.example.gadogado;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.gadogado.Fragments.HomeFragment;
+import com.example.gadogado.Fragments.ProfileFragment;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final int ID_HOME  = 1;
+    private final int ID_ADD  = 2;
+    private final int ID_PROFILE  = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchFragment(new HomeFragment());
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_ADD, R.drawable.ic_add));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_PROFILE, R.drawable.ic_person));
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if(model.getId() == ID_HOME){
+                    switchFragment(new HomeFragment());
+                } else if(model.getId() == ID_ADD){
+                    switchFragment(new HomeFragment());
+                } else if (model.getId() == ID_PROFILE){
+                    switchFragment(new ProfileFragment());
+                }
+                return null;
+            }
+        });
+
+        bottomNavigation.show(ID_HOME, true);
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
