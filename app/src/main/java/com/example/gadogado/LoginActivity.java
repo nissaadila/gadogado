67daff5bc9939404bde0c6aaf6d07bbf5d300776
nissaadila <nissaadila15@gmail.com>
2022-06-18 19:28:40 +0700

    update regist.java sama login.java baru inisialisasi belum firebase

--- app/src/main/java/com/example/gadogado/LoginActivity.java
+++ app/src/main/java/com/example/gadogado/LoginActivity.java
@@ -2,13 +2,57 @@ package com.example.gadogado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText emailTxt, passwordTxt;
+    TextView textViewRegister;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+
+        // firebase
+
+        textViewRegister.setOnClickListener(v ->{
+            Intent moveRegister = new Intent(LoginActivity.this,RegisterActivity.class);
+            startActivity(moveRegister);
+        });
+
+        buttonLogin.setOnClickListener(v -> {
+            validasi();
+            // firebase
+        });
+
+
+    }
+
+    private void validasi(){
+        String email = emailTxt.getText().toString();
+        String password = passwordTxt.getText().toString();
+
+        if(email.isEmpty()){
+            emailTxt.setError("Email must field");
+            emailTxt.requestFocus();
+        }
+        if(password.isEmpty()){
+            passwordTxt.setError("Password must field");
+            passwordTxt.requestFocus();
+        }
+        // validasi firebase
+    }
+
+    private void init(){
+        emailTxt = findViewById(R.id.editTextEmailLogin);
+        passwordTxt = findViewById(R.id.editTextPasswordLogin);
+        textViewRegister = findViewById(R.id.buttonRegisterDariLogin);
+        buttonLogin = findViewById(R.id.buttonLogin);
     }
 }
--- app/src/main/java/com/example/gadogado/RegisterActivity.java
+++ app/src/main/java/com/example/gadogado/RegisterActivity.java
@@ -2,13 +2,64 @@ package com.example.gadogado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText usernameText, emailText, passwordText;
+    TextView textViewLogin;
+    Button buttonRegister;
+
+    //tambah radio button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+
+        textViewLogin.setOnClickListener(v -> {
+            Intent moveLogin = new Intent(RegisterActivity.this,LoginActivity.class);
+            startActivity(moveLogin);
+        });
+
+        buttonRegister.setOnClickListener(v ->{
+            validasi();
+
+            //firebase
+
+        });
+    }
+
+    private void validasi(){
+
+        String username = usernameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (username.isEmpty()){
+            usernameText.setError("Username must field");
+            usernameText.requestFocus();
+        }
+        if (email.isEmpty()){
+            emailText.setError("Username must field");
+            emailText.requestFocus();
+        }
+        if (password.isEmpty()){
+            passwordText.setError("Username must field");
+            passwordText.requestFocus();
+        }
+
+    }
+    private void init(){
+        usernameText = findViewById(R.id.editTextUsernameRegister);
+        emailText = findViewById(R.id.editTextEmailRegister);
+        passwordText = findViewById(R.id.editTextPasswordRegister);
+        textViewLogin = findViewById(R.id.buttonLoginDariRegister);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
     }
 }
